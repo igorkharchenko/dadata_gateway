33bf5693a3c28510a65a7848324869a5d672e65b
Igor Kharchenko <hatand@bk.ru>
2025-05-28 03:28:57 +0700

    Init spring boot service

---
+++ src/main/java/com/hatand/dadatagateway/DadataGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hatand.dadatagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadataGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadataGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hatand/dadatagateway/DadataGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatand.dadatagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadataGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
