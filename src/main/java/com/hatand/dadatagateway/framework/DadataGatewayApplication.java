86b6f8f72c2749f39195e0b121f87006d3aab371
Igor Kharchenko <hatand@bk.ru>
2025-06-05 00:39:39 +0700

    Добавление основных файлов для запуска springboot-приложения

--- src/main/java/com/hatand/dadatagateway/framework/DadataGatewayApplication.java
+++ src/main/java/com/hatand/dadatagateway/framework/DadataGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.hatand.dadatagateway;
+package com.hatand.dadatagateway.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
